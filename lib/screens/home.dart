1533278f0d7723dba5e04a749d76d4a3912358ae
Masudbek <masud@Masudbeks-MacBook-Air.local>
2024-11-21 13:10:08 +0500

    another change

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:testapp/locations.dart';
 import 'package:testapp/data/data.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
+import 'package:testapp/models/map_point.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -10,16 +14,33 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late final YandexMapController _mapController;
+  YandexMapController? _mapController; // Changed to nullable
   double _mapZoom = 12.0; // Initial zoom level
-  final Point _initialPosition =
-      const Point(latitude: 41.3111, longitude: 69.2797); // Tashkent
 
   @override
   void dispose() {
-    _mapController.dispose();
+    _mapController?.dispose(); // Ensure proper cleanup of the controller
     super.dispose();
   }
+
+  Future<void> _initLocationLayer() async {
+    final locationPermissionIsGranted =
+        await Permission.location.request().isGranted;
+
+    if (locationPermissionIsGranted && _mapController != null) {
+      await _mapController!.toggleUserLayer(visible: true);
+    } else {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Нет доступа к местоположению пользователя'),
+          ),
+        );
+      });
+    }
+  }
+
+  CameraPosition? _userLocation;
 
   @override
   Widget build(BuildContext context) {
@@ -28,16 +49,32 @@ class _MapScreenState extends State<MapScreen> {
       body: Stack(
         children: [
           YandexMap(
-            onMapCreated: (controller) async {
-              _mapController = controller;
-              await _mapController.moveCamera(
-                CameraUpdate.newCameraPosition(
-                  CameraPosition(
-                    target: _initialPosition,
-                    zoom: _mapZoom,
+            onUserLocationAdded: (view) async {
+              if (_mapController == null) {} // Check if controller is null
+              // Get user location
+              _userLocation = await _mapController!.getUserCameraPosition();
+              // If location found, center the map on this point
+              if (_userLocation != null) {
+                await _mapController!.moveCamera(
+                  CameraUpdate.newCameraPosition(
+                    _userLocation!.copyWith(zoom: 10),
                   ),
+                  animation: const MapAnimation(
+                    type: MapAnimationType.linear,
+                    duration: 0.3,
+                  ),
+                );
+              }
+
+              return view.copyWith(
+                pin: view.pin.copyWith(
+                  opacity: 1,
                 ),
               );
+            },
+            onMapCreated: (controller) async {
+              _mapController = controller;
+              await _initLocationLayer();
             },
             onCameraPositionChanged: (cameraPosition, _, __) {
               setState(() {
@@ -52,31 +89,29 @@ class _MapScreenState extends State<MapScreen> {
           ),
           Positioned(
             right: 10,
-            bottom: 20,
-            child: Column(
-              children: [
-                FloatingActionButton(
-                  heroTag: 'zoom_in',
-                  onPressed: () async {
-                    _mapZoom = (_mapZoom + 1).clamp(0, 20); // Limit zoom level
-                    await _mapController.moveCamera(
-                      CameraUpdate.zoomTo(_mapZoom),
-                    );
-                  },
-                  child: const Icon(Icons.zoom_in),
-                ),
-                const SizedBox(height: 10),
-                FloatingActionButton(
-                  heroTag: 'zoom_out',
-                  onPressed: () async {
-                    _mapZoom = (_mapZoom - 1).clamp(0, 20); // Limit zoom level
-                    await _mapController.moveCamera(
-                      CameraUpdate.zoomTo(_mapZoom),
-                    );
-                  },
-                  child: const Icon(Icons.zoom_out),
-                ),
-              ],
+            bottom: 150,
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              onPressed: () async {
+                if (_mapController != null) {
+                  await _mapController!.moveCamera(
+                    CameraUpdate.newCameraPosition(
+                      _userLocation!.copyWith(zoom: 14.0),
+                    ),
+                    animation: const MapAnimation(
+                      type: MapAnimationType.linear,
+                      duration: 0.3,
+                    ),
+                  );
+                }
+              },
+              elevation: 3, // Set elevation
+              shape: const CircleBorder(), // Make the button round
+              child: SvgPicture.asset(
+                'assets/images/cursor.svg', // Set the SVG icon
+                width: 24, // Optional: Set width for the icon
+                height: 24, // Optional: Set height for the icon
+              ),
             ),
           ),
         ],
@@ -108,53 +143,76 @@ class _MapScreenState extends State<MapScreen> {
         );
       },
       onClusterTap: (self, cluster) async {
-        await _mapController.moveCamera(
-          animation: const MapAnimation(
-            type: MapAnimationType.linear,
-            duration: 0.3,
-          ),
-          CameraUpdate.newCameraPosition(
-            CameraPosition(
-              target: cluster.placemarks.first.point,
-              zoom: _mapZoom + 1,
+        if (_mapController != null) {
+          await _mapController!.moveCamera(
+            animation: const MapAnimation(
+              type: MapAnimationType.linear,
+              duration: 0.3,
             ),
-          ),
-        );
+            CameraUpdate.newCameraPosition(
+              CameraPosition(
+                target: cluster.placemarks.first.point,
+                zoom: _mapZoom + 1,
+              ),
+            ),
+          );
+        }
       },
     );
   }
-}
 
-/// Метод для генерации точек на карте
-List<MapPoint> _getMapPoints() {
-  return getMapPoints;
-}
+  /// Метод для генерации точек на карте
+  List<MapPoint> _getMapPoints() {
+    return getMapPoints; // Assuming `getMapPoints` is a function you have defined elsewhere.
+  }
 
-/// Метод для генерации объектов маркеров для отображения на карте
-List<PlacemarkMapObject> _getPlacemarkObjects(BuildContext context) {
-  return _getMapPoints()
-      .map(
-        (point) => PlacemarkMapObject(
-          mapId: MapObjectId('MapObject $point'),
-          point: Point(latitude: point.latitude, longitude: point.longitude),
-          opacity: 1,
-          icon: PlacemarkIcon.single(
-            PlacemarkIconStyle(
-              image: BitmapDescriptor.fromAssetImage(
-                'assets/images/mark.png',
+  /// Метод для генерации объектов маркеров для отображения на карте
+  List<PlacemarkMapObject> _getPlacemarkObjects(BuildContext context) {
+    return _getMapPoints()
+        .map(
+          (point) => PlacemarkMapObject(
+            mapId: MapObjectId('MapObject $point'),
+            point: Point(latitude: point.latitude, longitude: point.longitude),
+            opacity: 1,
+            icon: PlacemarkIcon.single(
+              PlacemarkIconStyle(
+                image: BitmapDescriptor.fromAssetImage(
+                  'assets/images/mark_large.png',
+                ),
+                scale: 0.5,
               ),
-              scale: 4,
             ),
+            onTap: (_, __) async {
+              // Move the camera to the tapped placemark with animation
+              if (_mapController != null) {
+                await _mapController!.moveCamera(
+                  CameraUpdate.newCameraPosition(
+                    CameraPosition(
+                      target: Point(
+                          latitude: point.latitude, longitude: point.longitude),
+                      zoom: _mapZoom +
+                          1, // Zoom in slightly when moving to the placemark
+                    ),
+                  ),
+                  animation: const MapAnimation(
+                    type: MapAnimationType.linear,
+                    duration: 0.3,
+                  ),
+                );
+              }
+
+              // Show the modal after camera animation is complete
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => _ModalBodyView(
+                  point: point,
+                ),
+              );
+            },
           ),
-          onTap: (_, __) => showModalBottomSheet(
-            context: context,
-            builder: (context) => _ModalBodyView(
-              point: point,
-            ),
-          ),
-        ),
-      )
-      .toList();
+        )
+        .toList();
+  }
 }
 
 /// Содержимое модального окна с информацией о точке на карте
